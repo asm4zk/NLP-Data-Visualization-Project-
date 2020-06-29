9ff2be4df548bae489e25f87eec14ce958b64c63
fstrafforello <francesco.strafforello.2000@gmail.com>
2020-06-29 16:57:37 -0400

    Update first graph json.

--- python-virtual-env/graphs.py
+++ python-virtual-env/graphs.py
@@ -2,6 +2,10 @@ import get_documents
 import os
 import sys
 import json
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import NMF
 
 def first_graph(word):
     """
@@ -20,16 +24,26 @@ def first_graph(word):
 
     tfidf = get_documents.TFIDF(jsons)
     relevant_documents = get_documents.get_associated_documents(tfidf, word)
+    coordinates_data_frame = data_frame_nmf_documents(tfidf)
 
-    d2 = {}
     for document in relevant_documents:
+        d2 = {}
         if document == relevant_documents[0]:
             d2["Title"] = document[0]
             d2["TFIDF"] = document[1][0]
+            for index, row in coordinates_data_frame.iterrows():
+                if row["json"] == document[0]:
+                    d2["x"] = row["principal component 1"]
+                    d2["y"] = row["principal component 2"]
+                    break
             d[word] = [d2]
         else:
             d2["Title"] = document[0]
             d2["TFIDF"] = document[1][0]
+            for index, row in coordinates_data_frame.iterrows():
+                if row["json"] == document[0]:
+                    d2["x"] = row["principal component 1"]
+                    d2["y"] = row["principal component 2"]
             d[word].append(d2)
     
     f = open("first_graph.json", "w")
@@ -37,6 +51,41 @@ def first_graph(word):
     f.close()
 
     return d
+
+def data_frame_nmf_documents(X):
+    jsons = X[:,0] # Get list of titles
+    jsons = np.delete(jsons, 0, 0) # Remove empty corner space
+    X = np.delete(X, 0, 1) # Delete the list of titles
+    words = X[0,:] # Get the list of words
+    X = np.delete(X, 0, 0) # Delete the list of words
+    X = X.astype('float') # Change type to float as there are only floats left
+
+    d = {} # Create new dictionary to create the data frame with pandas. Get words one by one as the keys and the list of frequencies per document as the values
+    for word in words:
+        d[word] = X[:,0]
+        X = np.delete(X, 0, 1)
+    d["json"] = jsons # Add the jsons titles at the end of the dictionary
+
+    data_frame = pd.DataFrame(d) # Create the data frame with pandas
+
+    x = data_frame.loc[:, words].values # Separate out the words
+
+    data = StandardScaler().fit_transform(x) # Standardize the words with the sklearn library
+
+    lowest = np.amin(data) # Get minimum value in data
+
+    for x in np.nditer(data, op_flags=['readwrite']): # Translate every value in data into a positive value by adding the absolute value of the lowest, keeping the distance between values
+        x[...] += abs(lowest)
+
+    nmf = NMF(n_components=2) # Create a 2-dimension nmf using the nmf method in the sklearn library
+
+    principalComponents = nmf.fit_transform(data) # Get the standardized data and put it in the nmf
+
+    principal_data_frame = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2']) # Create new data frame with the new adjustments
+
+    final_data_frame = pd.concat([principal_data_frame, data_frame[['json']]], axis = 1) # Add the json titles at then end of the data frame
+
+    return final_data_frame
 
 if __name__ == "__main__":
     first_graph("command")
